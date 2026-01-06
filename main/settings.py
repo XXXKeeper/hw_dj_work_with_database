59ad8ed7ba5b61503d0664ec15ffb48b061a1424
Артем Красноружских <akrasnoruzhskikh@krotech.ru>
2026-01-07 01:06:26 +0300

    change settings on default

--- main/settings.py
+++ main/settings.py
@@ -51,11 +51,11 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'artem',
+        'NAME': 'netology_import_phones',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'USER': 'artem',
-        'PASSWORD': '!@#qwe',
+        'USER': 'postgres',
+        'PASSWORD': 'ваш пароль',
     }
 }
 
