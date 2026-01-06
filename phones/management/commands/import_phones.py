9ced6c996dd30a164ea480379eb069e65e449f8b
Артем Красноружских <akrasnoruzhskikh@krotech.ru>
2026-01-07 01:05:21 +0300

    completed homework

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -1,6 +1,7 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
 from phones.models import Phone
 
 
@@ -14,4 +15,10 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            obj_phone = Phone(name=phone.get('name'),
+                              price=phone.get('price'),
+                              image=phone.get('image'),
+                              release_date=phone.get('release_date'),
+                              lte_exists=phone.get('lte_exists'),
+                              slug=slugify(phone.get('name')))
+            obj_phone.save()
--- phones/models.py
+++ phones/models.py
@@ -3,4 +3,9 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField()
+    price = models.IntegerField()
+    image = models.CharField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.CharField()
